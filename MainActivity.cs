1000c306d810477349414fc247dd665e94e60bfb
prashant360 <prashantm@360logica.com>
2016-08-29 15:48:59 +0530

    sdf

--- MainActivity.cs
+++ MainActivity.cs
@@ -21,6 +21,8 @@ namespace et3446
 			Button button = FindViewById<Button>(Resource.Id.myButton);
 
 			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+			Test
 		}
 	}
 }
